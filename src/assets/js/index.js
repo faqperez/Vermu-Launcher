b9f7fc3a56883945c8bf6c18ae48b90dffd877fa
Facundo Perez <faqperez@outlook.com>
2025-05-31 20:25:00 -0300

    Launcher 1.0.5
    Actualización en los textos dinámicos

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -53,14 +53,14 @@ class Splash {
     }
 
         async checkUpdate() {
-            this.setStatus(${t('auto_recherche_de_mise___jour')});
+            this.setStatus(t('auto_recherche_de_mise___jour'));
     
             ipcRenderer.invoke('update-app').then().catch(err => {
-                return this.shutdown(${t('auto_erreur_lors_de_la_recherche_de_mise___jour___br___')});
+                return this.shutdown(t('auto_erreur_lors_de_la_recherche_de_mise___jour___br___'));
             });
     
             ipcRenderer.on('updateAvailable', () => {
-                this.setStatus(${t('auto_mise___jour_disponible')});
+                this.setStatus(t('auto_mise___jour_disponible'));
                 if (os.platform() == 'win32') {
                     this.toggleProgress();
                     ipcRenderer.send('start-update');
@@ -107,7 +107,7 @@ class Splash {
             else if (os == 'linux') latest = this.getLatestReleaseForOS('linux', '.appimage', latestRelease);
     
     
-            this.setStatus(${t('auto_mise___jour_disponible___br__div_class__download_u')});
+            this.setStatus(t('auto_mise___jour_disponible___br__div_class__download_u'));
             document.querySelector(".download-update").addEventListener("click", () => {
                 shell.openExternal(latest.browser_download_url);
                 return this.shutdown("Téléchargement en cours...");
@@ -127,7 +127,7 @@ class Splash {
 
 
     startLauncher() {
-        this.setStatus(${t('auto_d_marrage_du_launcher')});
+        this.setStatus(t('auto_d_marrage_du_launcher'));
         ipcRenderer.send('main-window-open');
         ipcRenderer.send('update-window-close');
     }
